a24d9a9d34da288486fdcaeae223ddb851911a8b
MikhailGlukhov <140293905+MikhailGlukhov@users.noreply.github.com>
2024-10-15 11:57:11 +0700

    Delete fixing_car directory

--- fixing_car/android/app/src/main/kotlin/com/example/fixing_car/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fixing_car
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- fixing_car/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fixing_car/my_car_fixing.dart';
-import 'package:flutter/material.dart';
-
-void main(){
-  runApp(const MyCarFixing());
-}
